b335ea33fcc643c8feabd700dd1e31069f28c932
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-13 19:29:31 +0300

    deployed to heroku and not fnish yet

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+app.use(express.json({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server is started on the port " + PORT);
+});
