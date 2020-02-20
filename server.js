9b2f3a5c5a366eae2209f9fc269ba88fc2ed365e
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-20 23:10:49 +0300

    I am trying fix a request

--- server.js
+++ server.js
@@ -14,27 +14,34 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
-
-  app.options("/*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  app.options("/auth", function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
     );
     res.sendStatus(200);
   });
-
-  app.all("*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
-    );
-    next();
-  });
 }
+
+// app.options("/*", function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
+//   );
+//   res.sendStatus(200);
+// });
+
+// app.all("*", function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
+//   );
+//   next();
+// });
 
 app.use("/", require("./routes/quizRoute"));
 app.use("/users", require("./routes/userRoute"));
