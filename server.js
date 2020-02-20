8e71ba9d3dc2e00fdadf34fc8b26b07dbe318ea1
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-20 19:02:37 +0300

    I am trying fix a request

--- server.js
+++ server.js
@@ -15,21 +15,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.options("/*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  );
-  res.sendStatus(200);
-});
-
-app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 app.use("/", require("./routes/quizRoute"));
 app.use("/users", require("./routes/userRoute"));
 app.use("/auth", require("./routes/authRoute"));
