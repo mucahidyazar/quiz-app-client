193b6fb99debc05e2f214a961dfba5ec7d90dcee
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-20 23:46:30 +0300

    I am trying fix a request

--- server.js
+++ server.js
@@ -5,33 +5,34 @@ const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+app.use(express.json());
+
 app.use(express.json({ extended: false }));
+app.use(express.static("client/build"));
+
+app.options("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
+  );
+  res.sendStatus(200);
+});
+
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
+  );
+  next();
+});
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.json({ extended: false }));
-  app.use(express.static("client/build"));
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-  app.options("/*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
-    );
-    res.sendStatus(200);
-  });
-
-  app.all("*", function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
-    );
-    next();
   });
 }
 
