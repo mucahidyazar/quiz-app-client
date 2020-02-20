e78ee0b5e010ea43da82775fe52720b1c8758b5d
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-20 21:58:48 +0300

    I am trying fix a request

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.json({ extended: false }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
