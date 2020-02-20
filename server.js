a0bb3d8d54a68a6d6668b23010456b14fe709724
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-20 09:42:36 +0300

    fixed a problem

--- server.js
+++ server.js
@@ -7,28 +7,32 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello Worlde"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-// // Quizleri cekebilmek icin izin verdik CORS'a
-// app.options("/*", function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, Content-Length, X-Requested-With"
-//   );
-//   res.sendStatus(200);
-// });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
-// app.all("*", function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
+app.options("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  res.sendStatus(200);
+});
 
-//Defining Routes
-app.use("/auth", require("./routes/authRoute"));
-app.use("/users", require("./routes/userRoute"));
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use("/", require("./routes/quizRoute"));
+app.use("/users", require("./routes/userRoute"));
+app.use("/auth", require("./routes/authRoute"));
 
 app.listen(PORT, () => {
   console.log("Server is started on the port " + PORT);
