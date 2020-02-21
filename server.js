62d711d2893dc880a97be7225e9f0660c7b48499
Mucahid Yazar <mucahidyazar@gmail.com>
2020-02-21 11:19:29 +0300

    Test Heroku App

--- server.js
+++ server.js
@@ -5,30 +5,7 @@ const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
 connectDB();
-app.use(express.json());
-
 app.use(express.json({ extended: false }));
-app.use(express.static("client/build"));
-
-app.options("/*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
-  );
-  res.sendStatus(200);
-});
-
-app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
-  );
-  next();
-});
 
 if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
@@ -36,25 +13,25 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.options("/*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
-  );
-  res.sendStatus(200);
-});
+// app.options("/*", function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
+//   );
+//   res.sendStatus(200);
+// });
 
-app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
-  );
-  next();
-});
+// app.all("*", function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, Content-Length, X-Requested-With, x-auth-token"
+//   );
+//   next();
+// });
 
 app.use("/", require("./routes/quizRoute"));
 app.use("/users", require("./routes/userRoute"));
